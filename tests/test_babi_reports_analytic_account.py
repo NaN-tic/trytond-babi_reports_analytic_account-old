faba1e09d9096b0fc1c9b65da5dde286b2a0e6a4
resteve <resteve@zikzakmedia.com>
2015-12-01 09:43:04 +0100

    Tests

--- tests/test_babi_reports_analytic_account.py
+++ tests/test_babi_reports_analytic_account.py
@@ -1,24 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the babi_reports_analytic_account module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'babi_reports_analytic_account')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class BabiReportsAnalyticAccountTestCase(ModuleTestCase):
+    'Test Babi Reports Analytic Account module'
+    module = 'babi_reports_analytic_account'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        BabiReportsAnalyticAccountTestCase))
     return suite
